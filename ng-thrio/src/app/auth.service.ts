d37b336a7088bc284df927a4f2f09cc1fe19d3f8
odfalik <falik.oded.1@gmail.com>
2021-01-07 23:24:48 -0600

    auth service fg messaging

--- ng-thrio/src/app/auth.service.ts
+++ ng-thrio/src/app/auth.service.ts
@@ -7,7 +7,7 @@ import { AngularFireMessaging } from '@angular/fire/messaging';
 import { FunctionsService } from './functions.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   user: any;
@@ -19,23 +19,34 @@ export class AuthService {
     public auth: AngularFireAuth,
     private dbService: DbService,
     private afMessaging: AngularFireMessaging,
-    private fns: FunctionsService,
+    private fns: FunctionsService
   ) {
     this.auth.setPersistence('local').then(() => {
       this.auth.signInAnonymously();
     });
 
-    this.auth.user.subscribe(u => {
+    this.afMessaging.messages.subscribe(
+      (_messaging: AngularFireMessaging) => {
+      _messaging.onMessage = _messaging.onMessage.bind(_messaging);
+      _messaging.onTokenRefresh = _messaging.onTokenRefresh.bind(_messaging);
+    });
+
+    this.auth.user.subscribe((u) => {
       this.user = u;
       // console.log('user', u);
 
       if (this._dbUser) this._dbUser.unsubscribe();
       if (this.user) {
-        this._dbUser = this.dbService.getUser(this.user.uid).subscribe((dbUser: User) => {
-          this.dbUser = dbUser;
-        });
+        this._dbUser = this.dbService
+          .getUser(this.user.uid)
+          .subscribe((dbUser: User) => {
+            this.dbUser = dbUser;
+          });
 
-        if (!this.user.displayName) this.changeName('Guest' + Math.floor(Math.random() * Math.floor(10000)));
+        if (!this.user.displayName)
+          this.changeName(
+            'Guest' + Math.floor(Math.random() * Math.floor(10000))
+          );
       }
     });
   }
@@ -45,18 +56,25 @@ export class AuthService {
   }
 
   requestPermission(): void {
-      this.afMessaging.requestToken.subscribe(
-        (token) => {
-          this.fns.saveToken$({ token }).subscribe(() => {
-            this.pushRequest = false;
-          });
-        },
-        (error) => {
-          this.fns.saveToken$({ token: 'declined' }).subscribe(() => {
-            this.pushRequest = false;
-          });
-        }
-      );
+    this.afMessaging.requestToken.subscribe(
+      (token) => {
+        this.fns.saveToken$({ token }).subscribe(() => {
+          this.pushRequest = false;
+        });
+      },
+      (error) => {
+        this.fns.saveToken$({ token: 'declined' }).subscribe(() => {
+          this.pushRequest = false;
+        });
+      }
+    );
+  }
+
+  receiveMessage(): void {
+    this.afMessaging.messages.subscribe((payload) => {
+      console.log('new message received. ', payload);
+    // this.currentMessage.next(payload);
+    });
   }
 
   signOut(): Promise<void> {
