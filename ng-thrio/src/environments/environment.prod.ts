fbd9c6d8fb2df6c43e157f13f41dd31023e528d3
odfalik <falik.oded.1@gmail.com>
2021-01-28 00:16:04 -0600

    whoops

--- ng-thrio/src/environments/environment.prod.ts
+++ ng-thrio/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  useEmulators: true,
+  useEmulators: false,
   firebaseConfig: {
     apiKey: 'AIzaSyCwIVPOH_-HkveN0bRgvPXvi6zazGj2WWM',
     authDomain: 'thrio-d87c1.firebaseapp.com',
